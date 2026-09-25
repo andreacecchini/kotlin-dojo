db5540e4e4dbdd4d9b206a85690a5d9eb1570f5a
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-09-25 19:36:23 +0200

    feat: title and p

--- src/main/kotlin/dsl/html/Html.kt
+++ src/main/kotlin/dsl/html/Html.kt
@@ -3,7 +3,13 @@ package dsl.html
 interface Tag {
     val name: String
     val children: MutableList<Tag>
-    fun render(): String = "<$name>${children.joinToString("") { it.render() }}</$name>"
+    fun renderInner(): String = children.joinToString("") { it.render() }
+    fun render(): String = "<$name>${renderInner()}</$name>"
+}
+
+interface TagWithContent : Tag {
+    var content: String?
+    override fun renderInner(): String = (content ?: "") + super.renderInner()
 }
 
 class Html : Tag {
@@ -20,12 +26,40 @@ class Head : Tag {
     override val name: String
         get() = "head"
     override val children: MutableList<Tag> = mutableListOf()
+
+    fun title(init: Title.() -> Unit): Tag = Title().apply { init() }.also { children.add(it) }
 }
 
 class Body : Tag {
     override val name: String
         get() = "body"
     override val children: MutableList<Tag> = mutableListOf()
+
+    fun p(init: Paragraph.() -> Unit): Tag = Paragraph().apply { init() }.also { children.add(it) }
+}
+
+class Title : TagWithContent {
+    override val name: String
+        get() = "title"
+    override val children: MutableList<Tag>
+        get() = mutableListOf()
+    override var content: String? = null
+
+    operator fun String.unaryMinus() {
+        content = this
+    }
+}
+
+class Paragraph : TagWithContent {
+    override val name: String
+        get() = "p"
+    override val children: MutableList<Tag>
+        get() = mutableListOf()
+    override var content: String? = null
+
+    operator fun String.unaryMinus() {
+        content = this
+    }
 }
 
 fun html(init: Html.() -> Unit): Tag = Html().apply { init() }
@@ -33,8 +67,10 @@ fun html(init: Html.() -> Unit): Tag = Html().apply { init() }
 fun main() {
     html {
         head {
+            title { -"Kotlin Dsl" }
         }
         body {
+            p { -"This is a paragraph" }
         }
     }.render().also(::println)
 }
